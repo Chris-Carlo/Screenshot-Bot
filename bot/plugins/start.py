839450fb8983700855f5660ab45ebaca368c418b
DEVA NANDANAN B H <63557312+Chris-Carlo@users.noreply.github.com>
2020-09-01 16:05:57 +0530

    Update start.py

--- bot/plugins/start.py
+++ bot/plugins/start.py
@@ -15,17 +15,17 @@ async def start(c, m):
         )
     
     await m.reply_text(
-        text=f"Hi there {m.from_user.first_name}.\n\nI'm Screenshot Generator Bot. I can provide screenshots from your video files with out downloading the entire file (almost instantly). For more details check /help.",
+        text=f"Hi there {m.from_user.first_name}.\n\nI am Screenshot Generator Bot. I can provide screenshots from your video files with out downloading the entire file. For more details click /help.",
         quote=True,
         reply_markup=InlineKeyboardMarkup(
             [
                 [
-                    InlineKeyboardButton('📌  Support Group', url='https://t.me/InFoTelGroup'),
-                    InlineKeyboardButton('🔖  Projects Channel', url='https://t.me/TGBotsZ')
+                    InlineKeyboardButton('Group', url='https://t.me/CinemaCompanyGroup'),
+                    InlineKeyboardButton('Channel', url='https://t.me/CCALLMOVIESCHANNEL')
                 ],
                 [
-                    InlineKeyboardButton('💡  Source Code', url='https://github.com/TGExplore/Screenshot-Bot'),
-                    InlineKeyboardButton('👨  Master', url='https://t.me/odbots')
+                    InlineKeyboardButton('Support', url='https://t.me/darklester'),
+                    InlineKeyboardButton('Creator', url='https://t.me/Chris_Carlo')
                 ]
             ]
         )
